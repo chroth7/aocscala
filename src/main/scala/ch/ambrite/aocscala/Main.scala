a7e197fbf18cd40f9b040729ba8abc265ba76f13
Christian Roth <chroth@me.com>
2020-01-29 21:08:39 +0100

    Complete day3.

--- src/main/scala/ch/ambrite/aocscala/Main.scala
+++ src/main/scala/ch/ambrite/aocscala/Main.scala
@@ -3,17 +3,25 @@ package aocscala
 
 import Day1._
 import Day2._
+import Day3._
 
 object Main extends App {
-  println("─" * 10)
+  def divider() =  println("-" * 10)
+  divider()
 
   println("Day1_1: " + day1_1())
   println("Day1_2: " + day1_2())
 
-  println("─" * 10)
+  divider()
 
   println("Day2_1: " + day2_1(input_day2_1))
 
   println("Day2_2: " + day2_2())
 
+  divider()
+
+  println("Day3_1: " + day3_1(wire1Input, wire2Input))
+  println("Day3_2: " + day3_2(wire1Input, wire2Input))
+
+  divider()
 }
--- src/test/scala/ch/ambrite/aocscala/day3/Day3Suite.scala
+++ src/test/scala/ch/ambrite/aocscala/day3/Day3Suite.scala
@@ -4,10 +4,21 @@ package aocscala
 import Day3._
 
 class Day3Suite extends TestSuite {
-  test("Part 3") {
+  test("Making wires") {
     makeWire("R4") shouldBe Seq((0,4), (0,3), (0,2), (0,1), (0,0))
     makeWire("R4,U2") shouldBe Seq((2,4), (1,4), (0,4), (0,3), (0,2), (0,1), (0,0))
     makeWire("R4,U2,L2") shouldBe Seq((2,2), (2,3), (2,4), (1,4), (0,4), (0,3), (0,2), (0,1), (0,0))
     makeWire("R4,U2,L2,D5") shouldBe Seq((-3,2), (-2,2), (-1,2), (0,2), (1,2), (2,2), (2,3), (2,4), (1,4), (0,4), (0,3), (0,2), (0,1), (0,0))
+  }
+
+  test("Day3") {
+    day3_1("R8,U5,L5,D3", "U7,R6,D4,L4") shouldBe 6
+    day3_1("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83") shouldBe 159
+    day3_1("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7") shouldBe 135
+  }
+  test("Day3_2") {
+    day3_2("R8,U5,L5,D3", "U7,R6,D4,L4") shouldBe 30
+    day3_2("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83") shouldBe 610
+    day3_2("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7") shouldBe 410
   }
 }
